c9f8c09a55f0a274fb176da8ace86c1dff99ce7d
App Academy Student <student@appacademy.io>
2017-02-02 15:52:55 -0800

    finish two_sum

---
+++ two_sum.rb
@@ -0,0 +1,27 @@
+require 'byebug'
+def bad_two_sum?(arr, val)
+  #O(n^2)
+  (0...arr.length - 1).each do |i|
+    (i + 1... arr.length).each do |j|
+      return true if arr[i] + arr[j] == val
+    end
+  end
+  false
+end
+
+def okay_two_sum?(arr,val)
+  #O(nlogn)
+  sorted = arr.sort
+
+  until sorted.length == 1
+    case (sorted.first + sorted.last) <=> val
+    when 0
+      return true
+    when -1
+      sorted.shift
+    when 1
+      sorted.pop
+    end
+  end
+  false
+end
